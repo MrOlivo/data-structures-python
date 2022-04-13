d23ab91b2f0a39e6e6d377bc5e571c74f64ef1e2
Edwin Olivo <46498127+MrOlivo@users.noreply.github.com>
2022-04-13 17:22:03 -0600

    Update havelhakimi.py

--- grafos/havelhakimi.py
+++ grafos/havelhakimi.py
@@ -2,13 +2,15 @@ class HavelHakimi(object):
     def algorithm(self, arreglo):
         try:
             while arreglo:
-                arreglo.sort(reverse = True)
+                arreglo.sort(reverse=True)
                 print(arreglo)
                 d = arreglo.pop(0)
                 if d > len(arreglo):
-                    print('El valor (%s) es igual a la longitud (%s) del arreglo' % (d, len(arreglo)))
+                    print('El valor (%s) es igual a la longitud (%s) del arreglo' % (
+                        d, len(arreglo)))
                     break
-                if d == 0: break
+                if d == 0:
+                    break
                 for i in range(d):
                     arreglo[i] -= 1
         except:
