bf3215d9d3ed2998e1902aeb1574127ec6f8dc2a
Edwin Olivo <46498127+MrOlivo@users.noreply.github.com>
2022-04-13 16:36:48 -0600

    Update test_arboles.py

--- test/test_arboles.py
+++ test/test_arboles.py
@@ -1,10 +1,10 @@
-from arboles.arbol_binario import BinaryTree
 import os
 import sys
 
 sys.path.insert(0, os.path.abspath(
     os.path.join(os.path.dirname(__file__), "..")))
 
+from arboles.arbol_binario import BinaryTree
 
 tree = BinaryTree()
 
@@ -19,3 +19,4 @@ print("\nPREORDEN: ")
 tree.preorder()
 print("\nPOSTORDEN: ")
 tree.postorder()
+print("")
