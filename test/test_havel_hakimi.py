eb7bb6a003fbeb02a0601026b3e64a8a7abcd8cc
Edwin Olivo <46498127+MrOlivo@users.noreply.github.com>
2022-04-14 10:20:22 -0600

    Update test_havel_hakimi.py

--- test/test_havel_hakimi.py
+++ test/test_havel_hakimi.py
@@ -2,11 +2,21 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+sys.path.insert(0, os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "..")))
 
 from grafos.havelhakimi import HavelHakimi
 
 hk = HavelHakimi()
-hk.algorithm([2,4,4,5,5,5,6,7])
+
+# Example of Success
 print('{:*^25}'.format('*'))
-hk.algorithm([2,2,2,2,2,3,4,5])
+hk.algorithm([2, 4, 4, 5, 5, 5, 6, 7])
+
+# Example of Success
+print('{:*^25}'.format('*'))
+hk.algorithm([2, 2, 2, 2, 2, 3, 4, 5])
+
+# Example of Fail
+print('{:*^25}'.format('*'))
+hk.algorithm([2, 2, 2, 2, 2, 3, 9, 5])
